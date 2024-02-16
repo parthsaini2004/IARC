c657168818f45d1a2d0c03dc64277a213b295e9a
Piyush_Chavan <piyushchavangd2004@gmail.com>
2024-02-16 16:12:22 +0530

    Changes in responsiveness

--- src/components/initiatives/Alumini-bigwigs.js
+++ src/components/initiatives/Alumini-bigwigs.js
@@ -5,13 +5,13 @@ import img from './images/Screenshot 2024-01-23 211044 1.png';
 function AluminiBigwigs() {
 
     return (
-        <><br/><br/><br/><br/><br/><br/><br/><br/>
+        <>
             <div className='in-back'>
                 <div className='in-container'>
                     <h1 className='in-title'>Alumni Bigwigs</h1>
                     <p className='in-content'>The "Alumni Bigwigs" initiative is set to relaunch after a temporary hiatus. It entails the creation of engaging riddles and quizzes for IITR students. The primary aim is to enhance students' awareness of their esteemed alumni, inviting them to partake in a rewarding journey of discovery. As part of this initiative, small tokens of appreciation are planned for participating students, further enriching their engagement with the institute's alumni community.</p>
-                    <div style={{ display: 'flex', justifyContent: 'space-evenly', padding: '100px 0' }}>
-                        <img src={img} />
+                    <div style={{ display: 'flex', justifyContent: 'space-evenly', padding: '6.5vw 0' }}>
+                        <img style={{maxWidth:'60%'}} src={img} />
                     </div>
                 </div>
             </div>
