928855888182b347f18eaf91926484714d662df1
parthsaini2004 <129434562+parthsaini2004@users.noreply.github.com>
2024-03-07 21:20:00 +0530

    foo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -87,6 +87,7 @@ import {Link} from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 //hello
+////eth
 // function Navbar() {
 //     return (
 //         <>
