c5943a3fbedc622e6e646d4b8310d845dc74c514
Gowtham <138794663+Gouutham@users.noreply.github.com>
2024-03-07 21:45:51 +0530

    added changes

--- src/components/MeetTeam/MeetTeam.js
+++ src/components/MeetTeam/MeetTeam.js
@@ -1,32 +1,179 @@
-import React from 'react'
-import './MeetTeam.css'
-import image1 from './teamImages/image1.png'
-import image2 from './teamImages/image2.png'
-import image3 from './teamImages/image3.png'
-import image4 from './teamImages/image4.png'
-import image5 from './teamImages/image5.png'
-import image6 from './teamImages/image6.png'
-import image7 from './teamImages/image7.png'
-import image8 from './teamImages/image8.png'
-import image9 from './teamImages/image9.png'
-import image10 from './teamImages/image10.png'
-import image11 from './teamImages/image11.png'
-import linked from './teamImages/LinkedIn.png'
-import insta from './teamImages/Instagram.png'
+import React from "react";
+import "./MeetTeam.css";
+import image1 from "./teamImages/image1.png";
+import image2 from "./teamImages/image2.png";
+import image3 from "./teamImages/image3.png";
+import image4 from "./teamImages/image4.png";
+import image5 from "./teamImages/image5.png";
+import image6 from "./teamImages/image6.png";
+import image7 from "./teamImages/image7.png";
+import image8 from "./teamImages/image8.png";
+import image9 from "./teamImages/image9.png";
+import image10 from "./teamImages/image10.png";
+import image11 from "./teamImages/image11.png";
+import linked from "./teamImages/LinkedIn.png";
+import insta from "./teamImages/Instagram.png";
 
 export const MeetTeam = () => {
   return (
     <div>
-        <div className="teamm">
+      <div className="teamm">
         <p>Meet the Team</p>
-        <div className="cardbox"><div className='teamCard'><img src={image1} /><nmc className="nameCard"><h2>Pawanhaari Mittal</h2><h3>Secretary</h3><h5>email</h5><h5>+91 7728986489</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div></div>
+        <div className="cardbox">
+          <div className="teamCard">
+            <img src={image1} />
+            <nmc className="nameCard">
+              <h2>Pawanhaari Mittal</h2>
+              <h3>Secretary</h3>
+              <h5>email</h5>
+              <h5>+91 7728986489</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+        </div>
 
-        <div className="cardbox"><div className='teamCard'><img src={image2} /><nmc className="nameCard"><h2>Sanya Singh</h2><h3>Additional Secretary</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image3} /><nmc className="nameCard"><h2>Anand Seshadri</h2><h3>Additional Secretary</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div></div>
+        <div className="cardbox">
+          <div className="teamCard">
+            <img src={image2} />
+            <nmc className="nameCard">
+              <h2>Sanya Singh</h2>
+              <h3>Additional Secretary</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image3} />
+            <nmc className="nameCard">
+              <h2>Anand Seshadri</h2>
+              <h3>Additional Secretary</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+        </div>
 
-        <div className="cardbox"><div className='teamCard'><img src={image4} /><nmc className="nameCard"><h2>Krishna Verma</h2><h3>Joint Secretary (Operations)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image5} /><nmc className="nameCard"><h2>Saurav Kumar</h2><h3>Joint Secretary (Operations)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image6} /><nmc className="nameCard"><h2>Shivam Ojha</h2><h3>Joint Secretary (Editorial)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image7} /><nmc className="nameCard"><h2>Abhinav Goyal</h2><h3>Joint Secretary (Editorial)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image8} /><nmc className="nameCard"><h2>Paridhi Jain</h2><h3>Joint Secretary (Design)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image9} /><nmc className="nameCard"><h2>Sagar Kharat</h2><h3>Joint Secretary (Video Editing)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div></div>
+        <div className="cardbox">
+          <div className="teamCard ">
+            <img src={image4} />
+            <nmc className="nameCard">
+              <h2>Krishna Verma</h2>
+              <h3>Joint Secretary (Operations)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image5} />
+            <nmc className="nameCard">
+              <h2>Saurav Kumar</h2>
+              <h3>Joint Secretary (Operations)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image6} />
+            <nmc className="nameCard">
+              <h2>Shivam Ojha</h2>
+              <h3>Joint Secretary (Editorial)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image7} />
+            <nmc className="nameCard">
+              <h2>Abhinav Goyal</h2>
+              <h3>Joint Secretary (Editorial)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image8} />
+            <nmc className="nameCard">
+              <h2>Paridhi Jain</h2>
+              <h3>Joint Secretary (Design)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image9} />
+            <nmc className="nameCard">
+              <h2>Sagar Kharat</h2>
+              <h3>Joint Secretary (Video Editing)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+        </div>
 
-        <div className="cardbox"><div className='teamCard'><img src={image10} /><nmc className="nameCard"><h2>Krishna Gupta</h2><h3>Joint Secretary (Web-D)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img src={image11} /><nmc className="nameCard"><h2>Prachi Singh</h2><h3>Joint Secretary (Web-D)</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div></div>
+        <div className="cardbox">
+          <div className="teamCard">
+            <img src={image10} />
+            <nmc className="nameCard">
+              <h2>Krishna Gupta</h2>
+              <h3>Joint Secretary (Web-D)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+          <div className="teamCard">
+            <img src={image11} />
+            <nmc className="nameCard">
+              <h2>Prachi Singh</h2>
+              <h3>Joint Secretary (Web-D)</h3>
+              <h5>email</h5>
+              <h5>phone number</h5>
+              <conn>
+                <img src={linked} />
+                <img src={insta} />
+              </conn>
+            </nmc>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,44 +1,35 @@
-import React from 'react';
-import '../homepage/homepage.css';
-import imgphotoh from '../homepage/himg.png';
-import readmore from '../homepage/Frame 1421.png';
-import Profileimg from './imgchar';
+import React from "react";
+import "../homepage/homepage.css";
+import imgphotoh from "../homepage/himg.png";
+import readmore from "../homepage/Frame 1421.png";
+import Profileimg from "./imgchar";
 
-import seemore from '../homepage/seemore.png';
-import pic1 from '../homepage/pic1.png';
-import pic2 from '../homepage/pic2.png';
-import pic3 from '../homepage/pic3.png';
-import pic4 from '../homepage/pic4.png';
-import pic5 from '../homepage/pic5.png';
-import pic6 from '../homepage/pic6.png';
-import pic7 from '../homepage/pic7.png';
-import pic8 from '../homepage/pic8.png';
-import pic9 from '../homepage/pic9.png';
-import pic10 from '../homepage/pic10.png';
-import pic11 from '../homepage/pic11.png';
-import pic12 from '../homepage/pic12.png';
-import pic13 from '../homepage/pic13.png';
+import seemore from "../homepage/seemore.png";
+import pic1 from "../homepage/pic1.png";
+import pic2 from "../homepage/pic2.png";
+import pic3 from "../homepage/pic3.png";
+import pic4 from "../homepage/pic4.png";
+import pic5 from "../homepage/pic5.png";
+import pic6 from "../homepage/pic6.png";
+import pic7 from "../homepage/pic7.png";
+import pic8 from "../homepage/pic8.png";
+import pic9 from "../homepage/pic9.png";
+import pic10 from "../homepage/pic10.png";
+import pic11 from "../homepage/pic11.png";
+import pic12 from "../homepage/pic12.png";
+import pic13 from "../homepage/pic13.png";
 
-import image1 from '../MeetTeam/teamImages/image1.png'
-import image2 from '../MeetTeam/teamImages/image2.png'
-import image3 from '../MeetTeam/teamImages/image3.png'
-import othermem from './otherteam.png';
-import linked from '../MeetTeam/teamImages/LinkedIn.png'
-import insta from '../MeetTeam/teamImages/Instagram.png'
-
-
-
-
-
-
-
-
+import image1 from "../MeetTeam/teamImages/image1.png";
+import image2 from "../MeetTeam/teamImages/image2.png";
+import image3 from "../MeetTeam/teamImages/image3.png";
+import othermem from "./otherteam.png";
+import linked from "../MeetTeam/teamImages/LinkedIn.png";
+import insta from "../MeetTeam/teamImages/Instagram.png";
 
 const Homepage = () => {
-    return (
-        <>
-        <div className="homepage" id="homepage">
-
+  return (
+    <>
+      <div className="homepage" id="homepage">
         {/* <div id="my-carousel" className='carousel slide' data-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
@@ -65,10 +56,7 @@ const Homepage = () => {
         </div>
             */}
 
-
-
-
-{/*             
+        {/*             
 <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner ">
     <div className="carousel-item active">
@@ -92,87 +80,172 @@ const Homepage = () => {
   </button>
 </div> */}
 
+        <div id="carouselExampleIndicators" className="carousel slide">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={imgphotoh} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={imgphotoh} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={imgphotoh} className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
 
-<div id="carouselExampleIndicators" className="carousel slide">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={imgphotoh} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={imgphotoh} className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src={imgphotoh}  className="d-block w-100" alt="..." />
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-
-
-    <div className="aboutus">
-        <p>About Us</p>
-        <div className='aboutustext center '>The Institute Alumni Relations Cell, affectionately referred to as IARC serves as a connecting bridge between the expansive alumni community and their alma mater, IIT Roorkee. Operating under the aegis of the Dean of Resources and Alumni Affairs (DORA), IIT Roorkee, IARC is dedicated to its mission of fostering and nurturing an enduring connection and relationship between our cherished alumni and their beloved alma mater. This bond is meticulously cultivated through a myriad of events and programmes that IARC conducts with unwavering dedication.</div>
-        <div className="leftspace sizeread" ><a href="" ><img src={readmore} alt="" /></a></div>
-    </div>
-
+        <div className="aboutus">
+          <p>About Us</p>
+          <div className="aboutustext center ">
+            The Institute Alumni Relations Cell, affectionately referred to as
+            IARC serves as a connecting bridge between the expansive alumni
+            community and their alma mater, IIT Roorkee. Operating under the
+            aegis of the Dean of Resources and Alumni Affairs (DORA), IIT
+            Roorkee, IARC is dedicated to its mission of fostering and nurturing
+            an enduring connection and relationship between our cherished alumni
+            and their beloved alma mater. This bond is meticulously cultivated
+            through a myriad of events and programmes that IARC conducts with
+            unwavering dedication.
+          </div>
+          <div className="leftspace sizeread">
+            <a href="">
+              <img src={readmore} alt="" />
+            </a>
+          </div>
+        </div>
 
         <div className="galleryteamiarc">
           <p>Gallery</p>
           <div className="grouper">
-              <Profileimg  imgphoto={pic1} />
-              </div>
-              <div className="grouper">
-              <Profileimg  imgphoto={pic2} />
-              <Profileimg  imgphoto={pic3} />
-              </div>
-              <div className="grouper">
-              <Profileimg  imgphoto={pic4} />
-              <Profileimg  imgphoto={pic5} />
-              <Profileimg  imgphoto={pic6} />
-              </div>
-              <div className="grouper">
-              <Profileimg  imgphoto={pic7} />
-              <Profileimg  imgphoto={pic8} />
-              <Profileimg  imgphoto={pic9} />
-              <Profileimg  imgphoto={pic10} />
-              </div>
-              <div className="grouper">
-               <Profileimg  imgphoto={pic11} />
-               <Profileimg  imgphoto={pic12} />
-               <Profileimg  imgphoto={pic13} />
-               </div>
-               <div className="leftspace sizeread" ><a href="" ><img src={seemore} alt="" /></a></div>
-
-
+            <Profileimg imgphoto={pic1} />
+          </div>
+          <div className="grouper">
+            <Profileimg imgphoto={pic2} />
+            <Profileimg imgphoto={pic3} />
+          </div>
+          <div className="grouper">
+            <Profileimg imgphoto={pic4} />
+            <Profileimg imgphoto={pic5} />
+            <Profileimg imgphoto={pic6} />
+          </div>
+          <div className="grouper">
+            <Profileimg imgphoto={pic7} />
+            <Profileimg imgphoto={pic8} />
+            <Profileimg imgphoto={pic9} />
+            <Profileimg imgphoto={pic10} />
+          </div>
+          <div className="grouper">
+            <Profileimg imgphoto={pic11} />
+            <Profileimg imgphoto={pic12} />
+            <Profileimg imgphoto={pic13} />
+          </div>
+          <div className="leftspace sizeread">
+            <a href="">
+              <img src={seemore} alt="" />
+            </a>
+          </div>
         </div>
         <div className="teamm">
-        <p>Meet the Team</p>
-        <div className="cardbox"><div className='teamCard'><img className='hmimg' src={image1} /><nmc className="nameCard namhm"><h2>Pawanhaari Mittal</h2><h3>Secretary</h3><h5>email</h5><h5>+91 7728986489</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div></div>
+          <p>Meet the Team</p>
+          <div className="cardbox">
+            <div className="teamCard hmtcard">
+              <img className="hmimg" src={image1} />
+              <nmchome className="nameCard namhm">
+                <h2>Pawanhaari Mittal</h2>
+                <h3>Secretary</h3>
+                <h5>email</h5>
+                <h5>+91 7728986489</h5>
+                <conn>
+                  <img src={linked} />
+                  <img src={insta} />
+                </conn>
+              </nmchome>
+            </div>
+          </div>
 
-        <div className="cardbox hmcards"><div className='teamCard'><img className='hmimg' src={image2} /><nmc className="nameCard namhm"><h2>Sanya Singh</h2><h3>Additional Secretary</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div><div className='teamCard'><img className='hmimg' src={image3} /><nmc className="nameCard namhm"><h2>Anand Seshadri</h2><h3>Additional Secretary</h3><h5>email</h5><h5>phone number</h5><conn><img src={linked} /><img src={insta}/></conn></nmc></div></div>
-        <div className="leftspace sizeread othermemm" ><a href="/team" ><img src={othermem} alt="" /></a></div>
+          <div className="cardbox hmcards">
+            <div className="teamCard hmtcard">
+              <img className="hmimg" src={image2} />
+              <nmchome className="nameCard namhm">
+                <h2>Sanya Singh</h2>
+                <h3>Additional Secretary</h3>
+                <h5>email</h5>
+                <h5>phone number</h5>
+                <conn>
+                  <img src={linked} />
+                  <img src={insta} />
+                </conn>
+              </nmchome>
+            </div>
+            <div className="teamCard hmtcard">
+              <img className="hmimg" src={image3} />
+              <nmchome className="nameCard namhm">
+                <h2>Anand Seshadri</h2>
+                <h3>Additional Secretary</h3>
+                <h5>email</h5>
+                <h5>phone number</h5>
+                <conn>
+                  <img src={linked} />
+                  <img src={insta} />
+                </conn>
+              </nmchome>
+            </div>
+          </div>
+          <div className="leftspace sizeread othermemm">
+            <a href="/team">
+              <img src={othermem} alt="" />
+            </a>
+          </div>
         </div>
+      </div>
+    </>
+  );
+};
 
-
-     </div>
-
-
-      </>
-    )
-}
-
-export default Homepage
-
-
+export default Homepage;
