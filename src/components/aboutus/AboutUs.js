927f1c55df06b6b8e973a8e0f01b9f61197324c7
Gowtham <126971320+BlueeMew@users.noreply.github.com>
2024-03-23 16:31:01 +0530

    Add files via upload

---
+++ src/components/aboutus/AboutUs.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './aboutus.css'
+// import JOIN from '../homepage/Frame 14212.png';
+
+function AboutUs() {
+  return (
+    <div className="AboutContainer">
+      <div className="AboutBox1">
+        <div className="AboutBox-Heading1">About Us</div>
+        <div className="AboutBox-Content1">
+          <p>IARC stands for the Institute Alumni Relations Cell, which is dedicated to fostering connections and bridging the gap between the alma mater and esteemed alumni. Operating under the aegis of the Dean of Resources and Alumni, IARC has successfully built a vibrant student-alumni community and has undertaken numerous initiatives to fulfill its mission.
+          </p>
+          <br/>
+          <p>
+            Furthermore, IARC hosts several initiatives including Alumni Career Talks, Mock Interviews, R-land memories, Raoinzo, and various verticals that facilitate the organization's seamless operation.
+          </p>
+          <br/>
+          <p>
+            Content Vertical: The Content vertical manages all content and social media handles, promoting IARC through various channels and ensuring comprehensive dissemination of information through the power of words.
+          </p>
+          <br/>
+          <p>
+            Ops: The Operations vertical handles all groundwork and introduces new ideas to enhance the cell's functioning. It consistently oversees the necessary tasks and execution of operations within the cell.
+          </p>
+          <br/>
+          <p>
+            Design: The Design vertical creates visually appealing and informative posters for the institute and social media, ensuring the delivery of accurate content through appropriate visual and design strategies.
+          </p>
+          <br/>
+          <p>
+            Video: Our Video vertical manages all video and photo content, handling interviews, podcasts, testimonials, and introducing new initiatives to inform people about our latest offerings and developments.
+          </p>
+          <br/>
+          <p>
+            WebD: The Web Development vertical oversees the prestigious website of IARC and manages all necessary technical aspects.
+          </p>
+        </div>
+      </div>
+      <div className="AboutBox2">
+        <div className="AboutBox-Heading2">IARC Community</div>
+        <div className="AboutBox-Content2">
+          In addition to our alumni network, we boast an IARC community comprising over 1000 students. This community serves as a platform for students to stay connected with us and with each other. Within this community, participants share valuable opportunities and information for mutual benefit
+         
+        </div>
+        {/* <div className="joinimg" ><a href="" ><img src={JOIN} alt="" /></a></div> */}
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
